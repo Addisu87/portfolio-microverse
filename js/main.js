82821f099eb278da0e8bb8587e9897b2c103c531
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-05-10 01:40:53 +0300

    Modified mobile menu

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// const toggle = document.getElementById('toggle');
+// const nav = document.getElementById('nav');
+// toggle.addEventListener('click', () => {
+//   nav.classList.toggle('active');
+// });
+
+// Function to toggle the bar
+function myFunction() {
+  var x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
