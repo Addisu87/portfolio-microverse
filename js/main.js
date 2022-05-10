99eddce9a9f842b8092d541e8e86ff290200512a
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-05-10 19:27:21 +0300

    Incorporated  the issued comments on my file

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-const menuItem = document.querySelector('.mobile-item');
-const closeMobileItems = document.querySelectorAll('.closeMenu');
-function displayMobileMenu() {
-  menuItem.style.display = 'block';
+let mobileMenu = document.querySelector('#mobile-menu-hide');
+let btnMobileMenu = document.querySelector('#mobile-menu-button');
+let btnCancelMobileMenu = document.querySelector('#btn-close-nav');
+
+const PortfolioClicked = document.querySelector('.link-portfolio');
+const AboutClicked = document.querySelector('.link-about');
+const ContactClicked = document.querySelector('.link-contact');
+
+var page = document.getElementsByTagName('body')[0];
+
+function displaymobilemenu() {
+  mobileMenu.classList.replace('mobile-menu-hide', 'mobile-menu-show');
+  page.classList.add('noscroll');
 }
 
-function closeMobileMenu() {
-  menuItem.style.display = 'none';
+function hidemobilemenu() {
+  mobileMenu.classList.replace('mobil-menu-show', 'mobile-menu-hide');
+  page.classList.remove('noscroll');
 }
 
-document
-  .querySelector('.openMenu')
-  .addEventListener('click', displayMobileMenu);
-closeMobileItems.forEach((element) =>
-  element.addEventListener('click', closeMobileMenu)
-);
+btnMobileMenu.addEventListener('click', displaymobilemenu);
+btnCancelMobileMenu.addEventListener('click', hidemobilemenu);
+PortfolioClicked.addEventListener('click', hidemobilemenu);
+AboutClicked.addEventListener('click', hidemobilemenu);
+ContactClicked.addEventListener('click', hidemobilemenu);
