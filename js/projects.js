6ea43cee0fd8aed7229bf1d39ef31bc028ca9123
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-05-14 16:30:57 +0300

    Background of the popup window  made blured

--- js/projects.js
+++ js/projects.js
@@ -11,7 +11,7 @@ const projects = [
     technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
     popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
     seeLive: '#',
-    seeSource: '#',
+    seeSource: '#'
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ const projects = [
     technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
     popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
     seeLive: '#',
-    seeSource: '#',
+    seeSource: '#'
   },
   {
     id: 3,
@@ -39,7 +39,7 @@ const projects = [
     technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
     popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
     seeLive: '#',
-    seeSource: '#',
+    seeSource: '#'
   },
   {
     id: 4,
@@ -53,8 +53,8 @@ const projects = [
     technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
     popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
     seeLive: '#',
-    seeSource: '#',
-  },
+    seeSource: '#'
+  }
 ];
 
 /* Populate object to html elements when the page loads */
@@ -67,13 +67,13 @@ for (let i = 0; i < projects.length; i += 1) {
   }
 
   for (let k = 0; k < projects[i].experience.length; k += 1) {
-    experienceList
-      += k === 0
+    experienceList +=
+      k === 0
         ? `<li><a href="#" class="canopy">${projects[i].experience[k]}</a></li>`
         : `<li ><a href="#">${projects[i].experience[k]}</a></li>`;
     if (k !== projects[i].experience.length - 1) {
-      experienceList
-        += '<li><a href="#"><img src="/images/Counter.png" alt="dot"></a></li>';
+      experienceList +=
+        '<li><a href="#"><img src="/images/Counter.png" alt="dot"></a></li>';
     }
   }
 
@@ -104,6 +104,7 @@ for (let i = 0; i < projects.length; i += 1) {
 /* Begging of display popup modal */
 
 const popupModal = document.querySelector('#projectPopup');
+const body = document.querySelector('body');
 const displayPopupModal = (i) => {
   let techPop = '';
   let experienceList = '';
@@ -113,15 +114,18 @@ const displayPopupModal = (i) => {
   }
 
   for (let k = 0; k < projects[i].experience.length; k += 1) {
-    experienceList
-      += k === 0
+    experienceList +=
+      k === 0
         ? `<li><a href="#" class="canopy">${projects[i].experience[k]}</a></li>`
         : `<li ><a href="#">${projects[i].experience[k]}</a></li>`;
     if (k !== projects[i].experience.length - 1) {
-      experienceList
-        += '<li><a href="#"><img src="/images/Counter.png" alt="dot"></a></li>';
+      experienceList +=
+        '<li><a href="#"><img src="/images/Counter.png" alt="dot"></a></li>';
     }
   }
+
+  // disable the background scroll
+  body.style.overflow = 'hidden';
 
   document.querySelector('.projectPopup').innerHTML = `
     <div class="project-item">
@@ -162,14 +166,19 @@ const displayPopupModal = (i) => {
 
 const closePopupModal = () => {
   popupModal.style.display = 'none';
+  body.style.overflow = 'auto';
 };
 
-document.querySelectorAll('.btn-see-project').forEach((element) => element.addEventListener('click', () => {
-  displayPopupModal(element.getAttribute('data-index'));
-}));
+document.querySelectorAll('.btn-see-project').forEach((element) =>
+  element.addEventListener('click', () => {
+    displayPopupModal(element.getAttribute('data-index'));
+  })
+);
 
-document.querySelectorAll('.close').forEach((element) => element.addEventListener('click', () => {
-  closePopupModal(element.getAttribute('data-index'));
-}));
+document.querySelectorAll('.close').forEach((element) =>
+  element.addEventListener('click', () => {
+    closePopupModal(element.getAttribute('data-index'));
+  })
+);
 
 /* End of display popup modal */
